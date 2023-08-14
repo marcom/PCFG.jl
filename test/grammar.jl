6cd53533f007ec789198a5ef7121c9c1885cc176
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-08-14 19:00:45 +0200

    Improve test printing

--- test/grammar.jl
+++ test/grammar.jl
@@ -1,6 +1,14 @@
 using PCFGs: Grammar
 
 @testset "Grammar" begin
+    showtestset()
+
+    #g = Grammar(:S, :ε, [(:S, :ε, [:S, :S], 0.4), (:S, ['(', :S, ')'], 0.6)])
+    #@test g isa Grammar
+
+    #g = Grammar(:S, [(:S, [:S, :S], 0.4), (:S, ['(', :S, ')'], 0.6)])
+    #@test g isa Grammar
+
     g = Grammar([(:S, [:S, :S], 0.4), (:S, ['(', :S, ')'], 0.6)])
     @test g isa Grammar
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,10 @@
 using Test
 
-include("grammar.jl")
+# show which testset is currently running
+showtestset() = println(" "^(2 * Test.get_testset_depth()), "testing ",
+                        Test.get_testset().description)
+
+@testset verbose=true "PCFGs" begin
+    showtestset()
+    include("grammar.jl")
+end
